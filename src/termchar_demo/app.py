8c6e1320a86605d49417396ba274816f00e02003
David Fokkema <davidfokkema@icloud.com>
2024-07-05 11:30:02 +0200

    Send data from client to server

--- src/termchar_demo/app.py
+++ src/termchar_demo/app.py
@@ -1,33 +1,54 @@
+from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Container, Horizontal, Vertical
-from textual.widgets import Button, Input, Label, RichLog
+from textual.events import Event
+from textual.widgets import Button, Footer, Header, Input, Label, RichLog
 
 
-class Client(Container):
+class Device(Container):
+    class SendData(Event):
+        def __init__(self, data: str, *args, **kwargs) -> None:
+            super().__init__(*args, **kwargs)
+            self.data = data
+
+    BORDER_TITLE: str = "Device"
+
     def compose(self) -> ComposeResult:
-        with Vertical():
+        with Vertical(id="container"):
             yield Input(placeholder="Send characters", id="output")
-            yield RichLog()
+            yield (log := RichLog(id="log"))
+            log.border_title = "Application Log"
             yield (label := Label(id="input-buffer"))
             label.border_title = "Input Buffer"
 
+    @on(Input.Submitted)
+    def send_data(self, event: Input.Submitted) -> None:
+        self.post_message(self.SendData(event.input.value))
+        self.query_one(Input).clear()
 
-class Server(Container):
-    def compose(self) -> ComposeResult:
-        with Vertical():
-            yield (label := Label(id="input-buffer"))
-            label.border_title = "Input Buffer"
-            yield RichLog()
-            yield Input(placeholder="Send characters", id="output")
+
+class Client(Device):
+    BORDER_TITLE: str = "Client"
+
+
+class Server(Device):
+    BORDER_TITLE: str = "Server"
 
 
 class TermCharDemo(App[None]):
     CSS_PATH = "app.tcss"
 
     def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
         with Horizontal():
             yield Client()
             yield Server()
+
+    @on(Client.SendData)
+    def send_to_server(self, event: Client.SendData) -> None:
+        buffer: Label = self.query_one(Server).query_one("#input-buffer")
+        buffer.update(repr(event.data))
 
 
 TermCharDemo().run()
