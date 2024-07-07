bd0b6a49fc22a1d07b21e731c80eb26e5770d919
David Fokkema <davidfokkema@icloud.com>
2024-07-07 10:42:58 +0200

    Read from buffer with timeout

--- src/termchar_demo/app.py
+++ src/termchar_demo/app.py
@@ -3,22 +3,27 @@ from textual.app import App, ComposeResult
 from textual.containers import Container, Horizontal, Vertical
 from textual.events import DescendantBlur, Event
 from textual.reactive import reactive
+from textual.timer import Timer
 from textual.validation import Integer
 from textual.widgets import Button, Footer, Header, Input, Label, RichLog
 
 
 class Buffer(Label):
-    data: str
+    data = reactive("")
 
     def __init__(self, data: str = "", length: int = 30, *args, **kwargs) -> None:
         super().__init__(data, *args, **kwargs)
-        self.data = data
         self.length = length
+        self.data = data
         self.styles.max_width = self.length + 2
+
+    def render(self) -> str:
+        return self.data
 
     def append(self, new_data: str) -> None:
         self.data = (self.data + new_data)[: self.length]
-        self.update(self.data)
+
+    def watch_data(self):
         fill_factor = len(self.data) / self.length
         if fill_factor >= 0.8:
             self.set_classes("full")
@@ -27,11 +32,23 @@ class Buffer(Label):
         else:
             self.set_classes("")
 
+    def read(self, termchars: str) -> str | None:
+        if not termchars:
+            if self.data:
+                data = self.data
+                self.data = ""
+                return data
+            else:
+                return None
+
+        if termchars in self.data:
+            msg, self.data = self.data.split(termchars, maxsplit=1)
+            return msg
+        else:
+            return None
+
 
 class Device(Container):
-    is_busy_read = reactive(False)
-    timeout: int = 2
-
     class DataOut(Event):
         def __init__(self, data: str) -> None:
             super().__init__()
@@ -42,7 +59,16 @@ class Device(Container):
             super().__init__()
             self.data = data
 
+    class MessageRead(Event):
+        def __init__(self, msg: str) -> None:
+            super().__init__()
+            self.msg = msg
+
     BORDER_TITLE: str = "Device"
+
+    is_busy_reading = reactive(False)
+    timeout: int = 2
+    timeout_timer: Timer | None = None
 
     def compose(self) -> ComposeResult:
         with Vertical(id="container"):
@@ -72,17 +98,32 @@ class Device(Container):
         self.query_one("#read-termchars").border_title = "Read Termination Characters"
         self.query_one("#timeout").border_title = "Timeout"
 
-    def watch_is_busy_read(self, is_busy_read: bool) -> None:
+    def watch_is_busy_reading(self, is_busy_read: bool) -> None:
+        def unlock_read():
+            self.is_busy_reading = False
+            print(20 * "*" + "TRING")
+
         if is_busy_read:
             output: Input = self.query_one("#output")
             output.disabled = True
             output.placeholder = "Busy reading..."
             output.add_class("busy")
+            self.query_one("#read-button").disabled = True
+            self.query_one("#read-termchars").disabled = True
+            self.query_one("#timeout").disabled = True
+            self.timeout_timer = self.set_timer(
+                delay=self.timeout, callback=unlock_read
+            )
         else:
             output: Input = self.query_one("#output")
             output.disabled = False
             output.placeholder = "Type here to send data"
             output.remove_class("busy")
+            self.query_one("#read-button").disabled = False
+            self.query_one("#read-termchars").disabled = False
+            self.query_one("#timeout").disabled = False
+            if self.timeout_timer:
+                self.timeout_timer.stop()
 
     @on(Input.Submitted, "#timeout")
     @on(DescendantBlur, "#timeout")
@@ -94,11 +135,24 @@ class Device(Container):
 
     @on(Input.Submitted)
     @on(Button.Pressed, "#write-button")
-    def send_data(self) -> None:
+    def write(self) -> None:
         termchars: Input = self.query_one("#write-termchars").value
         output: Input = self.query_one("#output")
         self.post_message(self.DataOut(output.value + termchars))
         output.clear()
+
+    @on(Button.Pressed, "#read-button")
+    def read(self) -> None:
+        termchars: Input = self.query_one("#read-termchars").value
+        msg = self.query_one("#input-buffer").read(termchars=termchars)
+        if msg is not None:
+            self.post_message(self.MessageRead(msg))
+        else:
+            self.is_busy_reading = True
+
+    @on(MessageRead)
+    def log_read(self, event: MessageRead) -> None:
+        self.query_one("#log").write(f'[dark_olive_green1]> Read ← "{event.msg}"')
 
     @on(DataOut)
     def log_write(self, event: DataOut) -> None:
