9670e2cfaee404bb56ea0db2f52efd12fc36d4f7
David Fokkema <davidfokkema@icloud.com>
2024-07-16 14:57:44 +0200

    Removed variation selection character
    That character is invisible, should be invisible, but takes up space in Windows Terminal, breaking the layout.

--- src/termchar_demo/arrows.py
+++ src/termchar_demo/arrows.py
@@ -6,5 +6,5 @@ from textual.widgets import Label
 class Arrows(Container):
     def compose(self) -> ComposeResult:
         stem_length = int(self.styles.width.value) - 1
-        yield Label(stem_length * "─" + "▶︎", id="right")
-        yield Label("◀︎" + stem_length * "─", id="left")
+        yield Label(stem_length * "─" + "▶", id="right")
+        yield Label("◀" + stem_length * "─", id="left")
